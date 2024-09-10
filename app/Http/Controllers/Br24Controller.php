bd6b708178561d2b8cf62cc968bc41b5c0007989
dyogozzz <dyogozanetti@hotmail.com>
2024-09-10 18:30:21 -0300

    move redirect logic to br24 controller

---
+++ app/Http/Controllers/Br24Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Br24Controller extends Controller
+{
+    public function handlePost(Request $request)
+    {
+        $domain = $request->input('DOMAIN');
+        $protocol = $request->input('PROTOCOL');
+        $lang = $request->input('LANG');
+        $appSid = $request->input('APP_SID');
+
+        $redirectUrl = "https://frontend-for-integration.vercel.app/?DOMAIN={$domain}&PROTOCOL={$protocol}&LANG={$lang}&APP_SID={$appSid}";
+
+        return redirect()->to($redirectUrl, 307);
+    }
+}
