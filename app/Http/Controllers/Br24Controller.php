76a31fbbd779ce3fb7906cac2091c4684a4dbf07
dyogozzz <dyogozanetti@hotmail.com>
2024-09-16 15:30:45 -0300

    query list companys

--- app/Http/Controllers/Br24Controller.php
+++ app/Http/Controllers/Br24Controller.php
@@ -20,12 +20,12 @@ class Br24Controller extends Controller
     }
 
     public function geter(Request $request) {
-
-        $queryUrl = 'crm.contact.list';
+        $queryUrl = 'crm.company.list';
         $queryData = http_build_query(array(
-                
-        'select' => [ "ID", "NAME", "LAST_NAME", "UF_CRM_1588503377", "PHONE", "EMAIL"]
+                  
+        'select' => [ "ID","TITLE", "UF_CRM_1588503398",'UF_CRM_1588548671']
         ));
+        
         $result = Br24Conn::ConnWH($queryData, $queryUrl, 0) ;
         $result = json_decode($result, 1);
 
