e256f89e611159d56a608dbbf9a68041f843fa97
dyogozzz <dyogozanetti@hotmail.com>
2024-09-16 15:02:03 -0300

    turning method connwh static

--- app/Conn/Br24Conn.php
+++ app/Conn/Br24Conn.php
@@ -14,7 +14,7 @@ class Br24Conn
       return true;
   }
 
-  public function ConnWH($queryData, $queryUrl, $ssl_verify) {
+  public static function ConnWH($queryData, $queryUrl, $ssl_verify) {
     $queryUrl1 = 'https://b24-p96ng2.bitrix24.com.br/rest/1/joaysx7awoin2yh2/'.$queryUrl;
     $curl = curl_init();
 
