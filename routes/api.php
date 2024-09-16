b95709175b1dcd946f2f20678fbe8e64aa481113
dyogozzz <dyogozanetti@hotmail.com>
2024-09-16 14:19:16 -0300

    import controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Br24Controller;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
