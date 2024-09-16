325243a20b43b079008bc45d99c673505417c1f8
dyogozzz <dyogozanetti@hotmail.com>
2024-09-16 14:03:59 -0300

    webhook integration

---
+++ app/Conn/br24Conn.php
@@ -0,0 +1,42 @@
+<?php
+class Br24Conn
+{
+  public function writeToLog($data, $title = '') {
+      $log = "\n------------------------\n";
+      $log .= date("Y.m.d G:i:s") . "\n";
+      $log .= (strlen($title) > 0 ? $title : 'DEBUG') . "\n";
+      $log .= print_r($data, 1);
+      $log .= "\n------------------------\n";
+      file_put_contents(getcwd() . '/hook.log', $log, FILE_APPEND);
+      return true;
+  }
+
+  public function ConnWH($queryData, $queryUrl, $ssl_verify) {
+    $queryUrl1 = 'https://b24-p96ng2.bitrix24.com.br/rest/1/joaysx7awoin2yh2/'.$queryUrl;
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_SSL_VERIFYPEER => $ssl_verify,
+    CURLOPT_POST => 1,
+    CURLOPT_HEADER => 0,
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => $queryUrl1,
+    CURLOPT_POSTFIELDS => $queryData,
+    ));
+
+
+    $result = curl_exec($curl);
+    curl_close($curl);
+
+    return $result;
+  }
+
+  public function CompanyList() {
+    $queryUrl = 'crm.company.list';
+    $queryData = http_build_query(array(
+            'filter' => [ "UF_CRM_1588503398" => $cnpj ],
+            'select' => [ "UF_CRM_1588548671" ]
+        ));
+    $result = BitrixConn::ConnWH($queryData, $queryUrl, 0) ;
+  }
+}
--- app/Http/Controllers/Br24Controller.php
+++ app/Http/Controllers/Br24Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+require_once ('../Conn/br24Conn.php'); 
 
 class Br24Controller extends Controller
 {
@@ -16,6 +17,18 @@ class Br24Controller extends Controller
         $redirectUrl = "https://frontend-for-integration.vercel.app/?DOMAIN={$domain}&PROTOCOL={$protocol}&LANG={$lang}&APP_SID={$appSid}";
 
         return response('', 302)->header('Location', $redirectUrl);
-        // return redirect()->to($redirectUrl, 307);
+    }
+
+    public function geter(Request $request) {
+
+        $queryUrl = 'crm.contact.list';
+        $queryData = http_build_query(array(
+                
+        'select' => [ "ID", "NAME", "LAST_NAME", "UF_CRM_1588503377", "PHONE", "EMAIL"]
+        ));
+        $result = BitrixConn::ConnWH($queryData, $queryUrl, 0) ;
+        $result = json_decode($result, 1);
+
+        return response('', 200)->json();
     }
 }
